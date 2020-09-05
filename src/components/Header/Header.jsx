b86351b8593358c7532f5f7dc072992c68264574
DevYoma <Lawrenceyoma@gmail.com>
2020-09-05 01:40:33 +0100

    commiting

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return ( 
-     <h1 style={{margin: "none"}}>Covid-19 <span style={{ color: 'red'}}>Is real </span> <span style={{color: 'lightgreen'}}><i> Stay Safe 😷</i></span></h1>
+     <h1 style={{margin: "none"}}>Covid-19 <span style={{ color: 'red'}}>Is real </span> <span style={{color: 'lightgreen'}}> Stay Safe 😷</span></h1>
 
      );
 }

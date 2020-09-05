d524d0bad87ee4db668ae9b48368db7c34627e80
DevYoma <Lawrenceyoma@gmail.com>
2020-09-05 01:14:55 +0100

    modified the header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Header = () => {
     return ( 
-     <h1 style={{margin: "none"}}>Covid-19</h1>
+     <h1 style={{margin: "none"}}>Covid-19 <span style={{ color: 'red'}}>Is real </span> <span style={{color: 'lightgreen'}}><i> Stay Safe 😷</i></span></h1>
+
      );
 }
  
